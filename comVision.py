02342288dec9912d502d2786bddb6de3cab856cd
MikeDog <576268911@qq.com>
2016-01-29 20:55:23 +0800

    API test

---
+++ comVision.py
@@ -0,0 +1,33 @@
+import http.client, urllib.request, urllib.parse, urllib.error, base64
+from os import listdir
+from os.path import join
+
+
+headers = {
+    # Request headers
+    'Content-Type': 'application/octet-stream',
+    'Ocp-Apim-Subscription-Key': '5149efd4a1ad42c68e93b89e5294e87d',
+}
+
+params = urllib.parse.urlencode({
+    # Request parameters
+    'visualFeatures': 'All',
+})
+
+try:
+    conn = http.client.HTTPSConnection('api.projectoxford.ai')
+    FOLDER = "images/"
+    for filename in listdir(FOLDER):
+        print(filename, end = ', ')
+        full_path = join(FOLDER, filename)
+        # print(full_path)
+        img_bin = open(full_path, 'rb').read()
+        print(len(img_bin))
+        conn.request("POST", "/vision/v1/analyses?%s" % params, img_bin, headers)
+        response = conn.getresponse()
+        data = response.read()
+        print(data)
+    conn.close()
+except Exception as e:
+    print(e) # print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
